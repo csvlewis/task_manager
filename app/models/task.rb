91b341aee8674206d2b0b770359b8d05f1163772
Chris Lewis <13chlewis@gmail.com>
2019-01-16 13:29:39 -0700

    Added functionality to edit and delete tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,7 +15,6 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
@@ -24,9 +23,31 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE tasks
+                      SET title = ?,
+                          description = ?
+                      WHERE id = ?;",
+                      task_params[:title],
+                      task_params[:description],
+                      id)
+
+    Task.find(id)
+  end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM tasks
+                      WHERE id = ?;", id)
   end
 end
